c31e75b01ceab4152038991d16c2547f45bf0399
GalacticFrost <steenkampjean@gmail.com>
2024-09-25 09:22:26 +0200

    B: Phase 1 - Player: => Set the FPS of the game --> main.py => Imported sprites using pygame --> circleshape.py => Created Player Class and generated a Triangle sprite with circle hitbox --> player.py

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Asteriods-Video-Game
+Guided Project 2: Asteroids the Game
+
+This project aims to highlight principles of the following lessons:
+
+=> Python: Multiple module project (VSCode)
+=> Object Orientated Programming
+
+This project uses pygame 2.6.0 as a base for most of the core structure. The project has two 
+phases. 
+
+Phase 1 - Player: All modules and code that sets up the game window and the player 
+sprite/movement.
+
+Phase 2 - Asteroids: All modules and code that sets up the Asteroids, Player attacking, Asteroid
+'physics' (Breaking, Splitting, Collision).
+
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 
 	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))		#Generate a game window
+	clock = pygame.time.Clock()
+	dt = 0
+
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)					#Generate the player 
+
+	#Infinite loop - Generate gaming window.
 	while True:
-		screen.fill('black')
-		pygame.display.flip()
+		
+		screen.fill('black')					#Fill Screen Background
+		player.draw(screen)						#Draw player sprite every frame
+		pygame.display.flip()					#Refresh the display
 
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+			if event.type == pygame.QUIT:		#Close window - 'x' button	
 				return
+			
+		dt = clock.tick(60)/1000				#Set FPS to 60
+
 
 	print('Starting asteroids!')
 	print(f'Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}')
